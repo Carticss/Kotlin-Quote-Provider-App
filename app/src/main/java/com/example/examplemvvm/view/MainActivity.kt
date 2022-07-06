aa93b8ee6d7946b43ad55f31fb787f980b9ac6e7
Juan Diego Martinez <carticss@Juans-Mac-mini.local>
2022-07-06 11:38:16 -0500

    first commit

---
+++ app/src/main/java/com/example/examplemvvm/view/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.examplemvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import com.example.examplemvvm.databinding.ActivityMainBinding
+import com.example.examplemvvm.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val quoteViewModel : QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this,
+                Observer { currentQuote ->
+                    binding.tvQuote.text = currentQuote.quote
+                    binding.tvAuthor.text = currentQuote.author
+                }
+        )
+
+        quoteViewModel.randomQuote()
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+    }
+}
